1d7bb4eaff5eb80368d0750f59c0be638db91c4d
evanboissonnot <evan@boissonnot.fr>
2017-12-11 23:45:32 +0100

    Module 1 - Démarrage

---
+++ Formation csharp/R2D2pret/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2D2pret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Je suis R2D2, prêt pour le combat");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Formation csharp/R2D2pret2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2D2pret
+{
+    /// <summary>
+    /// Commentaire de classe
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Commentaire de méthode
+        /// </summary>
+        /// <param name="args">Argument</param>
+        static void Main(string[] args)
+        {
+            // Commentaire sur une ligne
+            Console.WriteLine("Je suis R2D2, prêt pour le combat");
+
+            /*
+             * Commentaire plus long
+             */
+            Console.ReadLine();
+        }
+    }
+}
