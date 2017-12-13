f567359dffc6a6bc82f2f2c3a3c8a9ac1cf38e44
evanboissonnot <evan@boissonnot.fr>
2017-12-13 15:44:51 +0100

    Ajout premier boss

---
+++ Formation csharp/PremierBoss/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PremierBoss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, je m’appelle C3PO");
+            /* 
+             * TODO: 01/12, On y reviendra plus tard
+             */
+        }
+    }
+}
